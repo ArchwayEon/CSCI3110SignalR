be111e4ad0ebcf225dbe2ffc581574305404adb0
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2023-12-07 12:54:49 -0500

    Added branch

--- CSCI3110SignalR/wwwroot/js/homeIndex.js
+++ CSCI3110SignalR/wwwroot/js/homeIndex.js
@@ -1,21 +1,5 @@
 'use strict';
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/applicationHub")
-    .build();
-
-connection.start().catch((err) => {
-    return console.error(err.toString());
-});
-
-// Set up the notification event listener
-connection.on("Notification", (message) => {
-    const incoming = JSON.parse(message);
-    if (incoming.type === "add number") {
-        populateWithRandomNumber(incoming.data);
-    }
-});
-
 const btnGetRandomNumber = document.querySelector("#btnGetRandomNumber");
 btnGetRandomNumber.addEventListener('click', (e) => {
     getRandomNumber();
@@ -30,9 +14,7 @@ function getRandomNumber() {
             return response.json();
         })
         .then(result => {
-            // This is also a connected client so will automatically update
-            // when the notification comes in (see above)
-            notifyConnectedClients("add number", result);
+            populateWithRandomNumber(incoming.data);
         });
 }
 
@@ -43,14 +25,4 @@ function populateWithRandomNumber(number) {
     ulRandomNumbers.appendChild(li);
 }
 
-function notifyConnectedClients(type, data) {
-    let message = {
-        type, data
-    };
-    console.log(JSON.stringify(message));
-    connection.invoke("SendMessageToAllAsync", JSON.stringify(message))
-        .catch(function (err) {
-            return console.error(err.toString());
-        });
-}
 
